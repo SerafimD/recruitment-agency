98b29afdea006fd0d42716e6da26527e3832bd4d
serafimd <serafimd@serafimd-desktop>
2016-12-05 23:05:11 +0300

    some fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :workers
   resources :vacancies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'vacancies#index'
+  get 'main', to: 'main#index', as: :main
+  root :to => 'main#index'
 end
