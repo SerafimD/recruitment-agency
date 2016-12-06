1c633d856ad5dfb1e05b7e661125380c903134d0
serafimd <serafimd@serafimd-desktop>
2016-12-06 23:38:55 +0300

    Add validations to worker model

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,2 +1,8 @@
 class Worker < ApplicationRecord
+  validates :name, :contact, presence: true
+  validates :salary, numericality: {greater_than: 0}
+  validates :name, format: {
+      with: %r{[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+},
+      message: 'Имя должно состоять из 3-х слов на кириллице'
+  }
 end
